fdbfc8c6f5ca1fef54868246c8f5f931ca376ae5
loganslowik168 <loganslowik168@gmail.com>
2024-10-04 21:39:44 -0500

    initial commit

---
+++ src/main/java/com/team5/cowboy_brothers/Cowboy_brothers.java
@@ -0,0 +1,7 @@
+package com.team5.cowboy_brothers;
+public class Cowboy_brothers {
+
+    public static void main(String[] args) {
+        System.out.println("-- Begin program execution --0");
+    }
+}
