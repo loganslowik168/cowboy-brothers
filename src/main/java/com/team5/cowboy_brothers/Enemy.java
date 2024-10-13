2f0debd2a8b7b06a468c6d15ffda31f0e202889b
matth <matth@Prometheus.home>
2024-10-13 12:25:06 -0500

    Basic Outline of Class

--- src/main/java/com/team5/cowboy_brothers/Enemy.java
+++ src/main/java/com/team5/cowboy_brothers/Enemy.java
@@ -5,9 +5,56 @@
 package com.team5.cowboy_brothers;
 
 /**
- *
+ *make an enemy class that follows a predefined path. instantiate several of these 
+ * enemies that come from the same enemy class but follow different paths. detect collision between any object and enemy.  
+ * on collision, print to console "HIT {object_id}" 
+ * or something similar. make some stationary object that the path will 
+ * collide with to demonstrate that it can collide with both players and terrain
  * @author matth
  */
 public class Enemy {
+    private String IDName;
+    private boolean alive;
+    private int species;
+    
+    public Enemy(){
+        IDName=null;
+        alive=false;
+        species=-1;
+    }
+    //Instantiator Has ID name for profiling, alive to know if to display them,
+    //and species value for type of path to walk and type of sprite to use
+    public Enemy(String s, int i){
+        IDName =s;
+        alive=true;
+        species=i;
+    }
+    
+    //To paint the temp sprite
+    public void create(){
+        
+    }
+    
+    //The predefined path will be set here
+    public void path(){
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
