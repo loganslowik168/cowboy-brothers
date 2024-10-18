f19df87c996be307bc2bb9449592f8b59e94a482
matth <matth@Prometheus>
2024-10-17 22:10:57 -0500

    Further made dbl linked list class

--- src/main/java/com/team5/cowboy_brothers/DblLinkList.java
+++ src/main/java/com/team5/cowboy_brothers/DblLinkList.java
@@ -10,8 +10,43 @@ package com.team5.cowboy_brothers;
  */
 public class DblLinkList {
     PathNode head;
+    PathNode tail;
     
-    public DblLinkList(PathNode head){
-        this.head=head;
+    public DblLinkList(){
+        head=null;
+        tail=null;
     }
+    // Traversing from head to the end of the list 
+public void traverseForward() 
+{ 
+	PathNode current = head; 
+	while (current != null) { 
+		System.out.print(current.data + " "); 
+		current = current.next; 
+	} 
+} 
+// Traversing from tail to the head 
+public void traverseBackward() 
+{ 
+	PathNode current = tail; 
+	while (current != null) { 
+		System.out.print(current.data + " "); 
+		current = current.prev; 
+	} 
+} 
+
+    public void insertAtEnd(int data) 
+{ 
+	PathNode temp = new PathNode(data); 
+	if (tail == null) { 
+		head = temp; 
+		tail = temp; 
+	} 
+	else { 
+		tail.next = temp; 
+		temp.prev = tail; 
+		tail = temp; 
+	} 
+} 
+
 }
