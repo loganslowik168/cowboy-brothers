c7662bb79f2e68063b72883c21b7fd4b2c9b4df8
matth <matth@Prometheus>
2024-10-20 14:24:53 -0500

    Final Commit before first Push

---
+++ src/main/java/com/team5/cowboy_brothers/Helper.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.team5.cowboy_brothers;
+
+/**
+ *
+ * @author matth
+ */
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.*;
+
+public class Helper extends TimerTask {
+    // Java program to demonstrate
+// scheduleAtFixedRate method of Timer class
+
+
+
+	public static int i = 0;
+	public void run()
+	{
+		
+		
+	}
+        public void run(Enemy s, Enemy f){
+            s.nextPosit();
+            f.nextPosit();
+	
+}
+
+
+}
