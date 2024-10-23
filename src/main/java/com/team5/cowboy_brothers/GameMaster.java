4c194ec1d7b530dd8f9439f15476350bf2523e15
matth <matth@Prometheus>
2024-10-22 20:22:27 -0500

    Master Class created

---
+++ src/main/java/com/team5/cowboy_brothers/GameMaster.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.team5.cowboy_brothers;
+
+/**
+ *
+ * @author matth
+ */
+public class GameMaster {
+    public void main(String[] args){
+        
+    }
+}
