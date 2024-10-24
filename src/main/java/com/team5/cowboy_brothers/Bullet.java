1fcaee7ff09054f54f024dc4d2c40bee19c66015
matth <matth@Prometheus>
2024-10-24 18:47:53 -0500

    Began Bullet Class

---
+++ src/main/java/com/team5/cowboy_brothers/Bullet.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.team5.cowboy_brothers;
+
+/**
+ *
+ * @author matth
+ */
+public class Bullet {
+    private final int InitVelocity = 10;
+    private int InitHeight;
+    
+    public void travel(){
+        //Projectile Motion to determine the coordinates of bullet
+    }
+    public void collision(){
+        //Check for collision with ground and be overriden for collision with Player/Enemy
+    }
+}
