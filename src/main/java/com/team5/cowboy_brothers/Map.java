a027e0a9b52ce5d30ff17be2a11b2dd720c92f1f
joshu <joshu@DESKTOP-N73UM1M>
2024-10-25 16:00:09 -0500

    Map super class

---
+++ src/main/java/com/team5/cowboy_brothers/Map.java
@@ -0,0 +1,23 @@
+package com.team5.cowboy_brothers;
+
+public abstract class Map {
+    protected int width;
+    protected int height;
+    protected Player player;
+
+    public Map(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    // Abstract method for loading map stats
+    public abstract void loadMapStats();
+
+    // Method to set the player
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    // Method to get the player's starting position (can be overridden in subclasses)
+    public abstract double[] getStartingPosition();
+}
