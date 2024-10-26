d92597f11bf7ba877657301573744b92575ed543
matth <matth@Prometheus>
2024-10-25 23:46:25 -0500

    Middle Progress of testing how to implement the timers

--- src/main/java/com/team5/cowboy_brothers/GameMaster.java
+++ src/main/java/com/team5/cowboy_brothers/GameMaster.java
@@ -3,13 +3,35 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.team5.cowboy_brothers;
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 /**
  *
  * @author matth
  */
 public class GameMaster {
-    public void main(String[] args){
+    Cowboy_bros_Menu n1 =  new Cowboy_bros_Menu();
+    
+    
+    
+    
+    public GameMaster(){
+       
+    }
+    
+    public static void main(String[] args){
+        //Cowboy_bros_Menu ka = new Cowboy_bros_Menu();
+        new GameMaster();
         
+    }
+    
+    
+    //time
+    public void time(){
+       //while(n1.getGameState()==GameState.GAMEPLAY){
+           
+       
     }
 }
