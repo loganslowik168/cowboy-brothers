56a60aa51aa447324d467fb5e8b4ff3901c5c6c9
matth <matth@Prometheus>
2024-10-26 19:34:54 -0500

    Added the imports, and the Bullet()

--- src/main/java/com/team5/cowboy_brothers/Bullet.java
+++ src/main/java/com/team5/cowboy_brothers/Bullet.java
@@ -2,7 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+
+
 package com.team5.cowboy_brothers;
+
+import javax.swing.*;
+import javax.swing.Timer;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  *
@@ -11,9 +20,16 @@ package com.team5.cowboy_brothers;
 public class Bullet {
     private final int InitVelocity = 10;
     private int InitHeight;
+    private int xPos,yPos;
+    
+    
+    public Bullet(){
+        
+    }
     
     public void travel(){
         //Projectile Motion to determine the coordinates of bullet
+        
     }
     public void collision(){
         //Check for collision with ground and be overriden for collision with Player/Enemy
