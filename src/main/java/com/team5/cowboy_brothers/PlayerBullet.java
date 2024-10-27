9b6da6b2c4849d5cc5226065304acbd3658de3e2
matth <matth@Prometheus>
2024-10-26 19:35:38 -0500

    Added the imports, and referenced the super travel method while leaving the collision ready to be created

--- src/main/java/com/team5/cowboy_brothers/PlayerBullet.java
+++ src/main/java/com/team5/cowboy_brothers/PlayerBullet.java
@@ -3,11 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.team5.cowboy_brothers;
+import javax.swing.*;
+import javax.swing.Timer;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  *
  * @author matth
  */
 public class PlayerBullet extends Bullet {
+    
+    public void travel(){
+        super.travel();
+    }
+    public void collision(){
+        //override to check for ground or enemies
+    }
+    
     
 }
