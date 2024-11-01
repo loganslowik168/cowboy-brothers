13d51b14230c66ea123fabce5617954434a77fcf
matth <matth@Prometheus>
2024-10-31 19:27:17 -0500

    Created pause instance for functional pausing and to store game data

---
+++ src/main/java/com/team5/cowboy_brothers/PauseButtonValue.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.team5.cowboy_brothers;
+
+/**
+ *Call constructor once pause button is pressed to save all current information of the lvl in progress
+ * Either use the saved values to recreate the game from the moment of the pause or reset level information
+ * @author matth
+ */
+
+public class PauseButtonValue {
+    
+    
+    
+    public PauseButtonValue(){
+        
+    }
+}
