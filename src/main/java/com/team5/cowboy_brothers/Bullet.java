2333bff70489be8824e9374a68780ec454f43be6
matth <matth@Prometheus>
2024-12-01 14:04:56 -0600

    Metting

--- src/main/java/com/team5/cowboy_brothers/Bullet.java
+++ src/main/java/com/team5/cowboy_brothers/Bullet.java
@@ -14,7 +14,7 @@ import javax.imageio.ImageIO;
 
 
 public abstract class Bullet extends MoveableGameObject{
-    protected int x, y; // Position of the bullet
+    //protected int x, y; // Position of the bullet
     private int speed; // Speed of the bullet
     private int direction; 
     private BufferedImage sprite, spriteR, spriteL;
@@ -41,6 +41,7 @@ public abstract class Bullet extends MoveableGameObject{
             default -> throw new IllegalArgumentException("Bullet irection must be 1 or -1");
         }
         System.out.println("Bullet size = " + this.width + "x" + this.height);
+        Cowboy_brothers.olly.gameWorld.moveableObjects.add(this);
         //Cowboy_brothers.olly.gameWorld.moveableObjects.add(this);
     }
     // Method to load the sprite
@@ -62,7 +63,7 @@ public abstract class Bullet extends MoveableGameObject{
     public void update() {
        // Update the bullet's position based on its speed and direction
        int travelOffset = speed * direction; // Move left or right
-       x += travelOffset + GetXOffset();
+       x += travelOffset ;
     }
 
     public void draw(Graphics g) {
